d078b894b0f24f582d1d740c7a743a5b847f31b2
David Kim <dkhw90@gmail.com>
2023-03-13 16:58:40 +0100

    Use leaflet to display map

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -10,3 +11,25 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+// https://www.google.com/maps/@39.326574,-2.4091331,7.3z
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const { latitude, longitude } = position.coords;
+
+    // Add leaflet map
+    const map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}. tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // Adds marker
+    const marker = L.marker([51.5, -0.09]).addTo(map);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+  }, function() {
+    alert('Could not get your position');
+  })
+}
